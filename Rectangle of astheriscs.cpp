f535718354dec3de14d55d550c20c20400f235b2
Oscar Antonio Hinojosa Salum <31547371+Caferino@users.noreply.github.com>
2017-11-07 12:47:30 -0600

    Add files via upload

---
+++ Rectangle of astheriscs.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int iRow, iColumn, iX, iY;
+
+int main()
+{
+    cout << "Insert the number of rows: ";
+    cin >> iRow;
+    cout << "Now, the columns: ";
+    cin >> iColumn;
+    iX=1;
+    while(iX<=iRow)
+    {
+        iY=1;
+        while(iY<=iColumn)
+        {
+            if(iX==1 || iY==1 || iColumn==iY || iX==iRow)
+            {
+                cout << "*";
+            }
+            else
+            {
+                cout << " ";
+            }
+            iY++;
+        }
+        cout << endl;
+        iX++;
+    }
+    return 0;
+
+}
